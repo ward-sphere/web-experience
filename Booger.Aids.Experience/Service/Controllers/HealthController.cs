c5a365be28a1b1d1693cdc1cfbded35dfeca41c7
Mason <mason@hicksm.dev>
2025-06-26 16:14:45 -0400

    Updated health check to monitor db connectivity

--- Booger.Aids.Experience/Service/Controllers/HealthController.cs
+++ Booger.Aids.Experience/Service/Controllers/HealthController.cs
@@ -1,14 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using Service.Infrastructure;
 
 namespace Service.Model
 {
-    public class HealthController : Controller
+    public class HealthController([FromServices] ExperienceContext ctx) : Controller
     {
+
+        private readonly ExperienceContext _context = ctx;
+
+        private void CheckDatabaseConnection()
+        {
+            if (_context.Database.CanConnect()) return;
+
+            string errorMessage = "Could not connect to database - check the"
+                + " AIDSBOOGER__PORTFOLIO__EXPERIENCE__CONNSTR environment"
+                + " variable and that database running";
+
+            throw new CheckFailedException(Results.Problem(errorMessage));
+        }
+
         [HttpGet("/")]
         [HttpGet("/health")]
         public IResult CheckHealth()
         {
+            try
+            {
+                CheckDatabaseConnection();
+            }
+            catch (CheckFailedException e)
+            {
+                return e.Result;
+            }
+
             return Results.Ok();
+        }
+
+        private class CheckFailedException(IResult result) : Exception
+        {
+            public IResult Result { get; set; } = result;
+
         }
     }
 }
