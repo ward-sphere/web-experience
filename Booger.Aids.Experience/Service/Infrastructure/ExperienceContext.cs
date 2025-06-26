6edc7aed57a6000d81a6bf7db95e1892dab16839
Mason <mason@hicksm.dev>
2025-06-26 15:54:39 -0400

    Moved configuration out of ExperienceContext

--- Booger.Aids.Experience/Service/Infrastructure/ExperienceContext.cs
+++ Booger.Aids.Experience/Service/Infrastructure/ExperienceContext.cs
@@ -3,17 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Service.Infrastructure
 {
-    public class ExperienceContext : DbContext
+    public class ExperienceContext(DbContextOptions<ExperienceContext> options) : DbContext(options)
     {
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(
-                System.Environment.GetEnvironmentVariable("PORTFOLIO_PGSQL_CONNECTION_STRING")
-            );
-        }
-
-
         public DbSet<Education> Educations { get; set; }
         public DbSet<Experience> Experiences { get; set; }
         public DbSet<ExperienceAchievement> ExperienceAchievements { get; set; }
--- Booger.Aids.Experience/Service/Program.cs
+++ Booger.Aids.Experience/Service/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Service.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ExperienceContext>(options =>
+    options.UseNpgsql(System.Environment.GetEnvironmentVariable("PORTFOLIO_PGSQL_CONNECTION_STRING")));
 
 var app = builder.Build();
 
